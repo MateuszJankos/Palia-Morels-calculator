feaf0f307bb935e0dbfa96e66988dabd7894ca40
MateuszJankos <jankowskim1002@gmail.com>
2023-10-15 20:43:59 +0200

    calculations

---
+++ simplecal.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter your glow worm farm count:   "))
+num2 = float(input("Enter how many morels left:   "))
+num3 = float(30)
+
+result = (num3 - num2) * num1
+print(round(result))
+
