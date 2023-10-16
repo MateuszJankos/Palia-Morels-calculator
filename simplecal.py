d2debb2ddbd51379b2c08d3f68c33ee5fea70923
MateuszJankos <jankowskim1002@gmail.com>
2023-10-16 23:13:10 +0200

    added stacks and rows

--- simplecal.py
+++ simplecal.py
@@ -3,4 +3,8 @@ num2 = float(input("Enter how many morels left:   "))
 num3 = float(30)
 
 result = (num3 - num2) * num1
-print(round(result))
+stacks = float(result / 30)
+rows = float(stacks / 8)
+print("Needed Morels: ", result)
+print("Stacks: ", stacks)
+print("Rows: ", rows)
