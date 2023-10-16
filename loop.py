c54a407d6393787a731e900663253b2a890cc12f
MateuszJankos <jankowskim1002@gmail.com>
2023-10-16 23:34:09 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,11 +1,11 @@
-num1 = float(input("Enter your glow worm farm count:   "))
-num2 = float(input("Enter how many morels left:   "))
+num1 = float(input("Enter your glow worm farm count:     "))
+num2 = float(input("Enter how many morels left inside:   "))
 num3 = float(30)
 
 result = (num3 - num2) * num1
 stacks = float(0)
 rows = float(0)
-print("Overall Morels:    ", result)
+print("Overall Morels:   ", result)
 
 while result >= 240:
     result = result - 240
