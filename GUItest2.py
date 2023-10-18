6e539d4f881dbfe0312c26dcc4bf9ce4af343d64
MateuszJankos <jankowskim1002@gmail.com>
2023-10-18 20:58:33 +0200

    Customtkinter version

--- GUItest2.py
+++ GUItest2.py
@@ -7,70 +7,49 @@ customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark",
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
 app = customtkinter.CTk()
-app.geometry("400x780")
-app.title("CustomTkinter simple_example.py")
+app.geometry("400x500")
+app.title("Morel Calculator")
 
 print(type(app), isinstance(app, tkinterDnD.Tk))
-
-def button_callback():
-    print("Button click", combobox_1.get())
-
-
-def slider_callback(value):
-    progressbar_1.set(value)
-
 
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT)
+label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, text="Palia morel calculator")
 label_1.pack(pady=10, padx=10)
 
-progressbar_1 = customtkinter.CTkProgressBar(master=frame_1)
-progressbar_1.pack(pady=10, padx=10)
-
-button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback)
-button_1.pack(pady=10, padx=10)
-
-slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
-slider_1.pack(pady=10, padx=10)
-slider_1.set(0.5)
-
-entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="CTkEntry")
+entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="Glow Worm count")
 entry_1.pack(pady=10, padx=10)
 
-optionmenu_1 = customtkinter.CTkOptionMenu(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-optionmenu_1.pack(pady=10, padx=10)
-optionmenu_1.set("CTkOptionMenu")
+entry_2 = customtkinter.CTkEntry(master=frame_1, placeholder_text="Morels left inside")
+entry_2.pack(pady=10, padx=10)
 
-combobox_1 = customtkinter.CTkComboBox(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-combobox_1.pack(pady=10, padx=10)
-combobox_1.set("CTkComboBox")
+def calculate_morels():
+    num1 = float(entry_1.get())
+    num2 = float(entry_2.get())
+    num3 = 30
 
-checkbox_1 = customtkinter.CTkCheckBox(master=frame_1)
-checkbox_1.pack(pady=10, padx=10)
+    result = (num3 - num2) * num1
+    stacks = 0
+    rows = 0
 
-radiobutton_var = customtkinter.IntVar(value=1)
+    while result >= 240:
+        result = result - 240
+        rows += 1
 
-radiobutton_1 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=1)
-radiobutton_1.pack(pady=10, padx=10)
+    while result >= 30:
+        result = result - 30
+        stacks += 1
 
-radiobutton_2 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=2)
-radiobutton_2.pack(pady=10, padx=10)
+button_1 = customtkinter.CTkButton(master=frame_1, command=calculate_morels, text="Calculate")
+button_1.pack(pady=10, padx=10)
 
-switch_1 = customtkinter.CTkSwitch(master=frame_1)
-switch_1.pack(pady=10, padx=10)
+label_2 = customtkinter.CTkLabel(master=frame_1, text="Rows of Morels: {rows}", fg_color="transparent",pady=10, padx=10).bind(command=calculate_morels)
 
-text_1 = customtkinter.CTkTextbox(master=frame_1, width=200, height=70)
-text_1.pack(pady=10, padx=10)
-text_1.insert("0.0", "CTkTextbox\n\n\n\n")
+label_3 = customtkinter.CTkLabel(master=frame_1, text="Stacks of Morels: {stacks}", fg_color="transparent",pady=10, padx=10).bind(command=calculate_morels)
 
-segmented_button_1 = customtkinter.CTkSegmentedButton(master=frame_1, values=["CTkSegmentedButton", "Value 2"])
-segmented_button_1.pack(pady=10, padx=10)
+label_4 = customtkinter.CTkLabel(master=frame_1, text="Single Morels: {result}", fg_color="transparent",pady=10, padx=10).bind(command=calculate_morels)
 
-tabview_1 = customtkinter.CTkTabview(master=frame_1, width=300)
-tabview_1.pack(pady=10, padx=10)
-tabview_1.add("CTkTabview")
-tabview_1.add("Tab 2")
+label_5 = customtkinter.CTkLabel(master=frame_1, text="Overall Morels: {result}", fg_color="transparent",pady=10, padx=10).bind(command=calculate_morels)
 
 app.mainloop()
